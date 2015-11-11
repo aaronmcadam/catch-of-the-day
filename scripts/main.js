f16180fd24422089b014e18510b7098508b706df
Aaron Mc Adam <aaron@aaronmcadam.com>
2015-11-11 20:25:48 +0000

    Adds form with interpolation

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 var React = require("react");
-var ReactDom = require("react-dom")
+var ReactDom = require("react-dom");
 
 var StorePicker = React.createClass({
   render: function() {
+    var name = "wes";
     return (
-      <p>hello</p>
+      <form className="store-selector">
+        <h2>Please Enter A Store {name}</h2>
+        <input type="text" ref="storeId" />
+        <input type="submit" />
+      </form>
     );
   }
 });
