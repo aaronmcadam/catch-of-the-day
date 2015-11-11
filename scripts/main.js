b7b8380154e2b33c62aef1d69341f496ba1c8891
Aaron Mc Adam <aaron@aaronmcadam.com>
2015-11-11 21:39:32 +0000

    Uses pushState for pretty URLs

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,7 @@ var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var Navigation = ReactRouter.Navigation;
+var createHistory = require("history/lib/createBrowserHistory");
 
 var App = React.createClass({
   render: function() {
@@ -64,7 +65,7 @@ var StorePicker = React.createClass({
 });
 
 var routes = (
-  <Router>
+  <Router history={createHistory()}>
     <Route path="/" component={StorePicker} />
     <Route path="/store/:storeId" component={App} />
   </Router>
