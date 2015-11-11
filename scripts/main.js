42741ba326d5f69343f5a755f85fa1941f9263e2
Aaron Mc Adam <aaron@aaronmcadam.com>
2015-11-11 20:12:20 +0000

    Adds simple StorePicker

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-alert("hi")
+var React = require("react");
+var ReactDom = require("react-dom")
+
+var StorePicker = React.createClass({
+  render: function() {
+    return (
+      <p>hello</p>
+    );
+  }
+});
+
+ReactDom.render(<StorePicker/>, document.querySelector("#main"));
